b6cc9be5c88679de7a9621ceeadfbd320fab82c0
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-22 12:44:00 +0900

    mobile frinedly

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,61 +6,61 @@ import arrow from "../images/down-arrow.svg";
 
 function Section({ title, leftBtnText,
   rightBtnText, Img1, Img2, Img3, projectTitle, projectDesc,
-   JavascriptIcon, ReactIcon, CSSIcon, ReduxIcon, HTMLIcon, FirebaseIcon,
-   NodeJsIcon, RubyIcon, RailsIcon }) {
-  return (
-    <Wrap>
+  JavascriptIcon, ReactIcon, CSSIcon, ReduxIcon, HTMLIcon, FirebaseIcon,
+  NodeJsIcon, RubyIcon, RailsIcon }) {
+    return (
+      <Wrap>
       <Fade top>
-        <ItemText>
-          <h1>{title}</h1>
-        </ItemText>
+      <ItemText>
+      <h1>{title}</h1>
+      </ItemText>
       </Fade>
       <ProjectContainer>
-        <ProjectInfo >
-          <h1>{projectTitle}</h1>
-          <p>{projectDesc}</p>
-          <IconContainer>
-            <Icons src={JavascriptIcon} />
-            <Icons src={ReactIcon} />
-            <Icons src={HTMLIcon} />
-            <Icons src={CSSIcon} />
-            <Icons src={NodeJsIcon} />
-            <Icons src={ReduxIcon} />
-            <Icons src={FirebaseIcon} />
-            <Icons src={RubyIcon} />
-            <Icons src={RailsIcon} />
-          </IconContainer>
-        </ProjectInfo>
-        <SliderContainer>
-          <ImgSlider
-            FirstImg={Img1}
-            SecondImg={Img2}
-            ThirdImg={Img3}
-          />
-        </SliderContainer>
+      <ProjectInfo >
+      <h1>{projectTitle}</h1>
+      <p>{projectDesc}</p>
+      <IconContainer>
+      <Icons src={JavascriptIcon} />
+      <Icons src={ReactIcon} />
+      <Icons src={HTMLIcon} />
+      <Icons src={CSSIcon} />
+      <Icons src={ReduxIcon} />
+      <Icons src={NodeJsIcon} />
+      <Icons src={FirebaseIcon} />
+      <Icons src={RubyIcon} />
+      <Icons src={RailsIcon} />
+      </IconContainer>
+      </ProjectInfo>
+      <SliderContainer>
+      <ImgSlider
+      FirstImg={Img1}
+      SecondImg={Img2}
+      ThirdImg={Img3}
+      />
+      </SliderContainer>
       </ProjectContainer>
       <Buttons>
-        <Fade bottom>
-          <ButtonGroup>
-            <LeftButton>
-              {leftBtnText}
-            </LeftButton>
-            {rightBtnText &&
-              <RightButton>
-                {rightBtnText}
-              </RightButton>
-            }
-          </ButtonGroup>
-        </Fade>
-        <DownArrow src={arrow} />
+      <Fade bottom>
+      <ButtonGroup>
+      <LeftButton>
+      {leftBtnText}
+      </LeftButton>
+      {rightBtnText &&
+        <RightButton>
+        {rightBtnText}
+        </RightButton>
+      }
+      </ButtonGroup>
+      </Fade>
+      <DownArrow src={arrow} />
       </Buttons>
-    </Wrap>
-  );
-}
+      </Wrap>
+      );
+    }
 
-export default Section;
+    export default Section;
 
-const Wrap = styled.div`
+    const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
     background-color: gray;
@@ -74,14 +74,14 @@ const Wrap = styled.div`
     justify-content: space-between;
     align-items: center;
     `;
-// sc for const shortcut
-const ItemText = styled.div`
+    // sc for const shortcut
+    const ItemText = styled.div`
     padding-top: 12vh;
     text-align: center;
     color: white;
     `;
 
-const ButtonGroup = styled.div`
+    const ButtonGroup = styled.div`
     display: flex;
     margin-bottom: 20px;
     cursor: pointer;
@@ -90,7 +90,7 @@ const ButtonGroup = styled.div`
     }
     `;
 
-const LeftButton = styled.div`
+    const LeftButton = styled.div`
     background-color: rgba(50, 50, 80, 0.8);
     height: 40px;
     width: 256px;
@@ -110,31 +110,35 @@ const LeftButton = styled.div`
     }
     `;
 
-const RightButton = styled(LeftButton)`
+    const RightButton = styled(LeftButton)`
     background-color: rgba(100, 50, 100, 0.8);
     `;
 
-const DownArrow = styled.img`
+    const DownArrow = styled.img`
     height: 40px;
     margin-bottom: 20px;
     overflow-x: hidden;
     animation: animateDown infinite 1.5s;
     `;
 
-const Buttons = styled.div`
+    const Buttons = styled.div`
 
     `;
 
-const ProjectContainer = styled.div`
+    const ProjectContainer = styled.div`
     border: 2px solid green;
     display: flex;
     justify-content: space-around;
     margin: 0px 60px;
     height: 50vh;
     width: 100vw;
+    @media (max-width : 768px) {
+      flex-direction: column;
+      height: 100vh;
+    }
     `;
 
-const SliderContainer = styled.div`
+    const SliderContainer = styled.div`
     border: 2px solid red;
     /* background-color: black; */
     max-width: 50%;
@@ -144,28 +148,36 @@ const SliderContainer = styled.div`
     justify-content: center;
     align-items: center;
     padding: -10px;
+    @media (max-width : 768px) {
+      max-width: 100%;
+      height: 40vh;
+    }
 
     `;
 
-const ProjectInfo = styled.div`
+    const ProjectInfo = styled.div`
     border: 2px solid blue;
-    max-width: 400px;
+    max-width: 450px;
     color: white;
     position: relative;
     `;
 
-const Icons = styled.img`
-    border: 2px solid pink;
-    height: 25px;
+    const Icons = styled.img`
+    /* border: 2px solid pink; */
+    height: 20px;
     cursor: pointer;
-    margin: 5px;
-    `;
+  `;
 
-const IconContainer = styled.div`
-    /* border: 2px solid yellow; */
-    display: flex;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    margin: 10px;
-    `;
+  const IconContainer = styled.div`
+  border: 2px solid yellow;
+
+  display: flex;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  margin: 5px;
+  @media (max-width : 768px) {
+      top: 0;
+      left: 0;
+    }
+  `;
