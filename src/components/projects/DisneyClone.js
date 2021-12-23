866d32c5ec79fc43db1c45d582b51b72d94a1745
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-23 23:36:43 +0900

    refactored all sections

--- src/components/projects/DisneyClone.js
+++ src/components/projects/DisneyClone.js
@@ -73,147 +73,148 @@ const Wrap = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  }`;
+}
+`;
 
 // sc for const shortcut
 const ItemText = styled.div`
-  /* border: 2px solid orange; */
-  width: 100vw;
-  padding-top: 12vh;
-  justify-content: center;
-  text-align: center;
-  color: white;
-  display: flex;
+/* border: 2px solid orange; */
+width: 100vw;
+padding-top: 12vh;
+justify-content: center;
+text-align: center;
+color: white;
+display: flex;
 
-  h1{
-    margin-top: -7px;
-    margin-right: 2px;
-    @media (max-width : 768px) {
-      font-size: 24px;
-      margin-top: -4px;
-    }
+h1{
+  margin-top: -7px;
+  margin-right: 2px;
+  @media (max-width : 768px) {
+    font-size: 24px;
+    margin-top: -4px;
   }
-    `;
+}
+`;
 
 const ButtonGroup = styled.div`
-    display: flex;
-    margin: 15px 10px;
-    cursor: pointer;
-    @media (max-width : 768px) {
-      flex-direction: column;
-    }
-    `;
+display: flex;
+margin: 15px 10px;
+cursor: pointer;
+@media (max-width : 768px) {
+  flex-direction: column;
+}
+`;
 
 const LeftButton = styled.div`
-    background-color: rgba(136, 217, 230, 1);
-    height: 40px;
-    width: 256px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    /* border: 0.1px solid black; */
-    border-radius: 100px;
-    opacity: 0.85;
-    text-transform: uppercase;
-    font-size: 12px;
-    margin: 8px;
-    transition: all 250ms ease;
+background-color: rgba(136, 217, 230, 1);
+height: 40px;
+width: 256px;
+display: flex;
+justify-content: center;
+align-items: center;
+/* border: 0.1px solid black; */
+border-radius: 100px;
+opacity: 0.85;
+text-transform: uppercase;
+font-size: 12px;
+margin: 8px;
+transition: all 250ms ease;
 
-    & a {
-      color: black;
-      text-decoration: none;
-    }
+& a {
+  color: black;
+  text-decoration: none;
+}
 
-    &:hover {
-      background-color: rgba(190, 226, 239, 1);
-    }
-    `;
+&:hover {
+  background-color: rgba(190, 226, 239, 1);
+}
+`;
 
 const RightButton = styled(LeftButton)`
-    `;
+`;
 
 
 const Buttons = styled.div`
-    /* border: 2px solid green; */
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    background: rgba(13, 24, 33, 0.9);
-    @media (max-width : 768px) {
-      max-width: 100%;
-      margin-top: 20px;
-    }
-    `;
+/* border: 2px solid green; */
+width: 100vw;
+display: flex;
+justify-content: center;
+background: rgba(13, 24, 33, 0.9);
+@media (max-width : 768px) {
+  max-width: 100%;
+  margin-top: 20px;
+}
+`;
 
 const ProjectContainer = styled.div`
-    /* border: 2px solid green; */
-    display: flex;
-    justify-content: space-around;
-    margin: 0px 60px;
-    height: 70vh;
-    width: 100vw;
-    @media (max-width : 768px) {
-      flex-direction: column;
-      height: 100vh;
-      max-width: 100%;
-    }
-    `;
+/* border: 2px solid green; */
+display: flex;
+justify-content: space-around;
+margin: 0px 60px;
+height: 70vh;
+width: 100vw;
+@media (max-width : 768px) {
+  flex-direction: column;
+  height: 100vh;
+  max-width: 100%;
+}
+`;
 
 const SliderContainer = styled.div`
-    /* border: 2px solid red; */
-    /* background-color: black; */
-    max-width: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: -10px;
-    @media (max-width : 768px) {
-      max-width: 100%;
-      display: flex;
-      margin-bottom: 100px;
-    }
-    `;
+/* border: 2px solid red; */
+/* background-color: black; */
+max-width: 50%;
+display: flex;
+justify-content: center;
+align-items: center;
+padding: -10px;
+@media (max-width : 768px) {
+  max-width: 100%;
+  display: flex;
+  margin-bottom: 100px;
+}
+`;
 
 const ProjectInfo = styled.div`
-    /* border: 2px solid blue; */
-    margin-left: 45px;
-    max-width: 500px;
-    color: white;
-    @media (max-width : 768px) {
-      max-width: 100%;
-      margin-left: 15px;
-      margin-top: 100px;
-    }
+/* border: 2px solid blue; */
+margin-left: 45px;
+max-width: 500px;
+color: white;
+@media (max-width : 768px) {
+  max-width: 100%;
+  margin-left: 15px;
+  margin-top: 100px;
+}
 
-    h2 {
-      margin-top: 30px;
-    }
+h2 {
+  margin-top: 30px;
+}
 
-    h4 {
-      margin-top: 10px;
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      @media (max-width : 768px) {
-        padding: 2px;
-        margin: 10px 10px;
-      }
-    }
+h4 {
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  @media (max-width : 768px) {
+    padding: 2px;
+    margin: 10px 10px;
+  }
+}
 
-    p {
-      text-align: justify;
-      @media (max-width : 768px) {
-        padding: 2px;
-        margin: 0 20px;
-      }
-    }
-    `;
+p {
+  text-align: justify;
+  @media (max-width : 768px) {
+    padding: 2px;
+    margin: 0 20px;
+  }
+}
+`;
 
 const Icons = styled.img`
-    /* border: 2px solid pink; */
-    height: 25px;
-    margin: 5px;
-    @media (max-width : 768px) {
-      height: 20px;
-    }
-    `;
+/* border: 2px solid pink; */
+height: 25px;
+margin: 5px;
+@media (max-width : 768px) {
+  height: 20px;
+}
+`;
--- src/components/projects/TeslaClone.js
+++ src/components/projects/TeslaClone.js
@@ -16,7 +16,12 @@ function Section({ leftBtnText, rightBtnText }) {
     <Wrap>
       <Fade top>
         <ItemText>
-          <h1>Tesla Clone</h1>
+          <h1>Tesla Clone |  </h1>
+            <Icons src={JavascriptIcon} />
+            <Icons src={ReactIcon} />
+            <Icons src={ReduxIcon} />
+            <Icons src={HTMLIcon} />
+            <Icons src={CSSIcon} />
         </ItemText>
       </Fade>
       <ProjectContainer>
@@ -30,13 +35,6 @@ function Section({ leftBtnText, rightBtnText }) {
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, officia natus? Necessitatibus dolorem provident fugit maxime debitis quam, a dolor quia labore voluptas enim, iusto, numquam nam dolores vitae voluptatibus!</p>
         </ProjectInfo>
         <SliderContainer>
-          <IconContainer>
-            <Icons src={JavascriptIcon} />
-            <Icons src={ReactIcon} />
-            <Icons src={ReduxIcon} />
-            <Icons src={HTMLIcon} />
-            <Icons src={CSSIcon} />
-          </IconContainer>
           <ImgSlider
             FirstImg={tc1}
             SecondImg={tc2}
@@ -65,130 +63,155 @@ function Section({ leftBtnText, rightBtnText }) {
 export default Section;
 
 const Wrap = styled.div`
-    min-width: 100vw;
+  max-width: 100vw;
   min-height: 100vh;
-    background-color: gray;
-    /* background-image: url("") */
-    /* ${props => `url("/images/${props.bgImage}")`}; */
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    `;
+  background-color: rgba(63, 124, 172, 0.8);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+`;
+
 // sc for const shortcut
 const ItemText = styled.div`
-    padding-top: 12vh;
-    text-align: center;
-    color: white;
-    `;
+/* border: 2px solid orange; */
+width: 100vw;
+padding-top: 12vh;
+justify-content: center;
+text-align: center;
+color: white;
+display: flex;
+
+h1{
+  margin-top: -7px;
+  margin-right: 2px;
+  @media (max-width : 768px) {
+    font-size: 24px;
+    margin-top: -4px;
+  }
+}
+`;
 
 const ButtonGroup = styled.div`
-    display: flex;
-    margin-bottom: 20px;
-    cursor: pointer;
-    @media (max-width : 768px) {
-      flex-direction: column;
-    }
-    `;
+display: flex;
+margin: 15px 10px;
+cursor: pointer;
+@media (max-width : 768px) {
+  flex-direction: column;
+}
+`;
 
 const LeftButton = styled.div`
-    background-color: rgba(50, 50, 80, 0.7);
-    height: 40px;
-    width: 256px;
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 100px;
-    opacity: 0.85;
-    text-transform: uppercase;
-    font-size: 12px;
-    margin: 8px;
+background-color: rgba(136, 217, 230, 1);
+height: 40px;
+width: 256px;
+display: flex;
+justify-content: center;
+align-items: center;
+/* border: 0.1px solid black; */
+border-radius: 100px;
+opacity: 0.85;
+text-transform: uppercase;
+font-size: 12px;
+margin: 8px;
+transition: all 250ms ease;
 
-    & a {
-      color: white;
-      text-decoration: none;
-    }
+& a {
+  color: black;
+  text-decoration: none;
+}
 
-    &:hover {
-      background-color: rgba(50, 50, 80, 0.9);
-    }
-    `;
+&:hover {
+  background-color: rgba(190, 226, 239, 1);
+}
+`;
 
 const RightButton = styled(LeftButton)`
-    background-color: rgba(100, 50, 100, 0.7);
+`;
 
-    &:hover {
-      background-color: rgba(100, 50, 100, 0.9) !important;
-    }
-    `;
 
 const Buttons = styled.div`
-
-    `;
+/* border: 2px solid green; */
+width: 100vw;
+display: flex;
+justify-content: center;
+background: rgba(13, 24, 33, 0.9);
+@media (max-width : 768px) {
+  max-width: 100%;
+  margin-top: 20px;
+}
+`;
 
 const ProjectContainer = styled.div`
-    /* border: 2px solid green; */
-    display: flex;
-    justify-content: space-around;
-    margin: 0px 60px;
-    height: 60vh;
-    width: 100vw;
-    @media (max-width : 768px) {
-      flex-direction: column;
-      height: 100vh;
-    }
-    `;
+/* border: 2px solid green; */
+display: flex;
+justify-content: space-around;
+margin: 0px 60px;
+height: 70vh;
+width: 100vw;
+@media (max-width : 768px) {
+  flex-direction: column;
+  height: 100vh;
+  max-width: 100%;
+}
+`;
 
 const SliderContainer = styled.div`
-    /* border: 2px solid red; */
-    /* background-color: black; */
-    max-width: 50%;
-    /* object-fit: contain; */
-    /* overflow: hidden; */
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: -10px;
-    @media (max-width : 768px) {
-      max-width: 100%;
-      height: 40vh;
-    }
-    `;
+/* border: 2px solid red; */
+/* background-color: black; */
+max-width: 50%;
+display: flex;
+justify-content: center;
+align-items: center;
+padding: -10px;
+@media (max-width : 768px) {
+  max-width: 100%;
+  display: flex;
+  margin-bottom: 100px;
+}
+`;
 
 const ProjectInfo = styled.div`
-    /* border: 2px solid blue; */
-    max-width: 500px;
-    color: white;
-    /* position: relative; */
-    margin-top: 20px;
-    @media (max-width : 768px) {
-      max-width: 100%
-    }
-    `;
+/* border: 2px solid blue; */
+margin-left: 45px;
+max-width: 500px;
+color: white;
+@media (max-width : 768px) {
+  max-width: 100%;
+  margin-left: 15px;
+  margin-top: 100px;
+}
+
+h2 {
+  margin-top: 30px;
+}
+
+h4 {
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  @media (max-width : 768px) {
+    padding: 2px;
+    margin: 10px 10px;
+  }
+}
+
+p {
+  text-align: justify;
+  @media (max-width : 768px) {
+    padding: 2px;
+    margin: 0 20px;
+  }
+}
+`;
 
 const Icons = styled.img`
-    /* border: 2px solid pink; */
-    height: 25px;
-    margin: 5px;
-    @media (max-width : 768px) {
-      height: 20px;
-    }
-    `;
-
-const IconContainer = styled.div`
-    /* border: 2px solid yellow; */
-
-    display: flex;
-    position: absolute;
-    top: 25px;
-    left: 20;
-    @media (max-width : 768px) {
-      top: 65px;
-      left: 20;
-    }
-    `;
+/* border: 2px solid pink; */
+height: 25px;
+margin: 5px;
+@media (max-width : 768px) {
+  height: 20px;
+}
+`;
