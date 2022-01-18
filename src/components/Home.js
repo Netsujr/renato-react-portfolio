2930e4d847357d5de0a542819ac47e6daea8f6ec
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-18 17:04:27 +0900

    added Tada on click. Changed Home to class

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,65 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
+import Tada from 'react-reveal/Tada';
 import styled from 'styled-components';
 import renato from '../images/renatobg.png';
+import JavascriptIcon from "../images/Icons/javascript-plain.svg";
+import ReactIcon from "../images/Icons/react-original.svg";
+import FirebaseIcon from "../images/Icons/firebase-plain-wordmark.svg";
+import HTMLIcon from "../images/Icons/html5-original.svg";
+import CSSIcon from "../images/Icons/css3-original.svg";
+import ReduxIcon from "../images/Icons/redux-original.svg";
+import HerokuIcon from "../images/Icons/heroku-original-wordmark.svg";
+import RubyIcon from "../images/Icons/ruby-original-wordmark.svg";
+import RailsIcon from "../images/Icons/rails-plain-wordmark.svg";
+import NodeIcon from "../images/Icons/nodejs-original.svg";
 
-function Home() {
 
-  return (
-    <Container>
-      <ItemText>
-        <h1>Renato Delboni || Web Developer</h1>
-      </ItemText>
-      <InfoContainer>
-        <Fade left>
-          <ImgContainer>
-            <img src={renato} alt="" />
-          </ImgContainer>
-        </Fade>
-        <Fade center
-        config={{ delay: 2000, duration: 29000 }}>
-          <DetailsContainer>
-            <h1>Hello</h1>
-          </DetailsContainer>
-        </Fade>
-      </InfoContainer>
-    </Container >
-  );
+
+
+class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { counter: 0 };
+  }
+  handleClick() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  render() {
+    return (
+      <Container>
+        <ItemText>
+          <h1>Renato Delboni || Web Developer</h1>
+        </ItemText>
+        <InfoContainer>
+          <Fade left>
+            <ImgContainer>
+              <img src={renato} alt="" />
+            </ImgContainer>
+          </Fade>
+          <Fade center>
+            <DetailsContainer>
+              <Tada spy={this.state.counter}>
+                <Icons src={JavascriptIcon} onClick={this.handleClick} />
+                <Icons src={ReactIcon} />
+                <Icons src={FirebaseIcon} />
+                <Icons src={HTMLIcon} />
+                <Icons src={CSSIcon} />
+                <Icons src={NodeIcon} />
+                <Icons src={RubyIcon} />
+                <Icons src={RailsIcon} />
+                <Icons src={ReduxIcon} />
+                <Icons src={HerokuIcon} />
+              </Tada>
+            </DetailsContainer>
+          </Fade>
+        </InfoContainer>
+      </Container >
+    );
+  }
 }
 
 export default Home;
@@ -87,4 +122,13 @@ const DetailsContainer = styled.div`
   justify-content: center;
   align-items: center;
 
+  `;
+
+const Icons = styled.img`
+  /* border: 2px solid pink; */
+  height: 55px;
+  margin: 5px;
+  @media (max-width : 768px) {
+    height: 20px;
+  }
   `;
