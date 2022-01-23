bfdae29542fecdc873f76b05f692c4dc92365189
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-23 20:53:43 +0900

    added panels to homepage!

--- src/components/Home.js
+++ src/components/Home.js
@@ -34,29 +34,29 @@ class Home extends React.Component {
         <ItemText>
           <h1>Renato Delboni || Web Developer</h1>
         </ItemText>
-        <InfoContainer>
-          <Fade left>
-            <ImgContainer>
-              <img src={renato} alt="" />
-            </ImgContainer>
-          </Fade>
-          <Fade center>
-            <DetailsContainer>
-              <Tada spy={this.state.counter}>
-                <Icons src={JavascriptIcon} onClick={this.handleClick} />
-                <Icons src={ReactIcon} />
-                <Icons src={FirebaseIcon} />
-                <Icons src={HTMLIcon} />
-                <Icons src={CSSIcon} />
-                <Icons src={NodeIcon} />
-                <Icons src={RubyIcon} />
-                <Icons src={RailsIcon} />
-                <Icons src={ReduxIcon} />
-                <Icons src={HerokuIcon} />
-              </Tada>
-            </DetailsContainer>
-          </Fade>
-        </InfoContainer>
+        <Panels>
+          <Panel id="panel1">
+            <p>Hey</p>
+            <p>Let's</p>
+            <p>Dance</p>
+          </Panel>
+          <Panel id="panel2">
+            <p>Give</p>
+            <p>Take</p>
+            <p>Receive</p>
+          </Panel>
+          <Panel id="panel3">
+            <p>Experience</p>
+            <p>It</p>
+            <p>Today</p>
+          </Panel>
+          <Panel id="panel4">
+            <p>Give</p>
+            <p>All</p>
+            <p>You can</p>
+          </Panel>
+        </Panels>
+
       </Container >
     );
   }
@@ -69,6 +69,44 @@ const Container = styled.div`
   height: 100%;
   border: 2px solid orange;
   object-fit: contain;
+  `;
+
+const Panels = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  display: flex;
+
+  #panel1 {
+    background-image: url("https://source.unsplash.com/gYl-UtwNg_I/1500x1500");
+  }
+  #panel2 {
+    background-image: url("https://source.unsplash.com/rFKUFzjPYiQ/1500x1500");
+  }
+  #panel3 {
+    background-image: url("https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&w=1500&h=1500&fit=crop&s=967e8a713a4e395260793fc8c802901d");
+  }
+  #panel4 {
+    background-image: url("https://source.unsplash.com/ITjiVXcwVng/1500x1500");
+  }
+  `;
+
+const Panel = styled.div`
+  background: #6b0f9c;
+  box-shadow: inset 0 0 0 5px rgba(255, 255, 255, 0.1);
+  color: white;
+  text-align: center;
+  align-items: center;
+  transition: font-size 0.7s cubic-bezier(0.61, -0.19, 0.7, -0.11),
+  flex 0.7s cubic-bezier(0.61, -0.19, 0.7, -0.11), background 0.2s;
+  font-size: 20px;
+  background-size: cover;
+  background-position: center;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
   `;
 
 const InfoContainer = styled.div`
@@ -112,10 +150,10 @@ const ImgContainer = styled.div`
   }
   `;
 
-const DetailsContainer = styled.div`
+const IconsContainer = styled.div`
   border: 2px solid blue;
-  width: 64vw;
-  height: 100vh;
+  max-width: 100%;
+  max-height: 100%;
   padding: 10px;
   margin: 10px;
   display: flex;
@@ -126,9 +164,40 @@ const DetailsContainer = styled.div`
 
 const Icons = styled.img`
   /* border: 2px solid pink; */
-  height: 55px;
+  height: 35px;
   margin: 5px;
   @media (max-width : 768px) {
     height: 20px;
   }
   `;
+
+
+
+// ----------------------------------------------------------------------------------------------------------------
+
+{/* <Fade center>
+    <IconsContainer>
+    <Tada spy={this.state.counter}>
+    <Icons src={JavascriptIcon} onClick={this.handleClick} />
+    <Icons src={ReactIcon} />
+    <Icons src={FirebaseIcon} />
+    <Icons src={HTMLIcon} />
+    <Icons src={CSSIcon} />
+    <Icons src={NodeIcon} />
+    <Icons src={RubyIcon} />
+    <Icons src={RailsIcon} />
+    <Icons src={ReduxIcon} />
+    <Icons src={HerokuIcon} />
+    </Tada>
+    </IconsContainer>
+    </Fade>
+
+    <InfoContainer>
+    <Fade left>
+    <ImgContainer>
+    <img src={renato} alt="" />
+    </ImgContainer>
+    </Fade>
+    </InfoContainer>
+
+  */}
