c74fa5db5991efe8416128a61f0277ea9423ec30
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-03 14:30:41 +0900

    Finalized refactoring

--- src/components/PersonalProjects/ProjectDetails/SpotifyClone.js
+++ src/components/PersonalProjects/ProjectDetails/SpotifyClone.js
@@ -16,8 +16,8 @@ function SpotifyClone({ leftBtnText, rightBtnText }) {
       leftBtnText={leftBtnText}
       rightBtnText={rightBtnText}
       role="For this project I was again using a similar authentication login as I did with the Disney Clone project, however, this time I was calling Spotify's API to connect to user's info."
-      problem="The point of the App was to be able to control your songs with basic functionality like changing to a next or previous song. I had a lot fun going deeper into Spotify's API capabilities. One little issue I had was matching the album cover to the current song playing, I used the index to find the correct cover."
-      solution="I'm still debugging this album/song issue. the rest of the app was really fun to build and get to actually work! Please share if you have any ideas!"
+      problems="The point of the App was to be able to control your songs with basic functionality like changing to a next or previous song. I had a lot fun going deeper into Spotify's API capabilities. One little issue I had was matching the album cover to the current song playing, I used the index to find the correct cover."
+      solutions="I'm still debugging this album/song issue. the rest of the app was really fun to build and get to actually work! Please share if you have any ideas!"
       img1={sp1}
       img2={sp2}
       img3={sp3}
--- src/components/PersonalProjects/ProjectDetails/TravelAdvisor.js
+++ src/components/PersonalProjects/ProjectDetails/TravelAdvisor.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-import ImgSlider from '../../ImgSlider';
-import Fade from 'react-reveal/Fade';
 import ta1 from "../../../images/PersonalProjects/TravelAdvisor/one.png";
 import ta2 from "../../../images/PersonalProjects/TravelAdvisor/two.png";
 import ta3 from "../../../images/PersonalProjects/TravelAdvisor/three.png";
@@ -15,216 +12,24 @@ import AllProjects from './AllProjects';
 
 function TravelAdvisor({ leftBtnText, rightBtnText }) {
   return (
-    <Wrap>
-      <Fade top>
-        <ItemText>
-          <h1>Travel Advisor |  </h1>
-          {/*Put Icons inside Itemtext */}
-          <Icons src={JavascriptIcon} />
-          <Icons src={ReactIcon} />
-          <Icons src={HTMLIcon} />
-          <Icons src={CSSIcon} />
-          <Icons src={GoogleCloud} />
-          <Icons src={RapidAPI} />
-        </ItemText>
-      </Fade>
-      <ProjectContainer>
-        <ProjectInfo >
-          <h2>Details</h2>
-          <h4>Role:</h4>
-          <p>A solo project I had an idea for to practice calling and using different APIs in a single page application. I was also practicing the implementation of CSS using a different method, that is MakeStyles, although I decided to revert back to styledComponents as it seemed cleaner for me. </p>
-          <h4>Problems:</h4>
-          <p> I had a few issues with the layout and grid of the app. Working with MaterialUI classes and trying to implement your own style in the end became too difficult. I realised that MUI could be used for mostly quick projects, without thinking about style too much. Being a front end engineer, I could not work with it so well.   </p>
-          <h4>Solutions:</h4>
-          <p>I changed back to styledComponents where I had full control of the style. This meant losing some of the MUI functionality. For example, I'm still working on bringing the hovered card to the front, something that MUI was doing automatically for me. Not a huge deal, but something I want to add back. </p>
-        </ProjectInfo>
-        <SliderContainer>
-          <ImgSlider
-            FirstImg={ta1}
-            SecondImg={ta2}
-            ThirdImg={ta3}
-          />
-        </SliderContainer>
-      </ProjectContainer>
-      <Buttons>
-        <Fade bottom>
-          <ButtonGroup>
-            <LeftButton>
-              {leftBtnText}
-            </LeftButton>
-            {rightBtnText &&
-              <RightButton>
-                {rightBtnText}
-              </RightButton>
-            }
-          </ButtonGroup>
-        </Fade>
-      </Buttons>
-    </Wrap>
+    <AllProjects
+      title="Travel Advisor |  "
+      leftBtnText={leftBtnText}
+      rightBtnText={rightBtnText}
+      role="A solo project I had an idea for to practice calling and using different APIs in a single page application. I was also practicing the implementation of CSS using a different method, that is MakeStyles, although I decided to revert back to styledComponents as it seemed cleaner for me."
+      problems="I had a few issues with the layout and grid of the app. Working with MaterialUI classes and trying to implement your own style in the end became too difficult. I realized that MUI could be used for mostly quick projects, without thinking about style too much. Being a front end engineer, I could not work with it so well."
+      solutions="I changed back to styledComponents where I had full control of the style. This meant losing some of the MUI functionality. For example, I' m still working on bringing the hovered card to the front, something that MUI was doing automatically for me.Not a huge deal, but something I want to add back."
+      img1={ta1}
+      img2={ta2}
+      img3={ta3}
+      icon1={JavascriptIcon}
+      icon2={ReactIcon}
+      icon3={HTMLIcon}
+      icon4={CSSIcon}
+      icon5={GoogleCloud}
+      icon6={RapidAPI}
+    />
   );
 }
 
 export default TravelAdvisor;
-
-const Wrap = styled.div`
-  max-width: 100vw;
-  min-height: 100vh;
-  background-color: rgba(248, 241, 255, 0.8);
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-}
-`;
-
-// sc for const shortcut
-const ItemText = styled.div`
-/* border: 2px solid orange; */
-width: 100vw;
-padding-top: 12vh;
-justify-content: center;
-text-align: center;
-color: black;
-display: flex;
-
-
-h1{
-  margin-top: -7px;
-  margin-right: 2px;
-  @media (max-width : 768px) {
-    font-size: 24px;
-    margin-top: -4px;
-  }
-}
-`;
-
-const ButtonGroup = styled.div`
-display: flex;
-margin: 15px 10px;
-cursor: pointer;
-@media (max-width : 768px) {
-  flex-direction: column;
-}
-`;
-
-const LeftButton = styled.div`
-background-color: rgba(9, 21, 64, 0.8);
-height: 40px;
-width: 256px;
-display: flex;
-justify-content: center;
-align-items: center;
-/* border: 0.1px solid black; */
-border-radius: 100px;
-opacity: 0.85;
-text-transform: uppercase;
-font-size: 12px;
-margin: 8px;
-transition: all 250ms ease;
-
-& a {
-  color: white;
-  text-decoration: none;
-}
-
-&:hover {
-  background-color: rgba(9, 21, 64, 0.9);
-}
-`;
-
-const RightButton = styled(LeftButton)`
-`;
-
-
-const Buttons = styled.div`
-/* border: 2px solid green; */
-width: 100vw;
-display: flex;
-justify-content: center;
-background: rgba(48, 197, 255, 0.4);
-@media (max-width : 768px) {
-  max-width: 100%;
-  margin-top: 20px;
-  @media (max-width : 468px) {
-    margin-top: 100px;
-  }
-}
-`;
-
-const ProjectContainer = styled.div`
-/* border: 2px solid green; */
-display: flex;
-justify-content: space-around;
-margin: 0px 60px;
-height: 70vh;
-width: 100vw;
-@media (max-width : 768px) {
-  flex-direction: column;
-  height: 100vh;
-  max-width: 100%;
-}
-`;
-
-const SliderContainer = styled.div`
-/* border: 2px solid red; */
-/* background-color: black; */
-max-width: 50%;
-display: flex;
-justify-content: center;
-align-items: center;
-padding: -10px;
-@media (max-width : 768px) {
-  max-width: 100vh;
-  display: flex;
-  margin-bottom: 100px;
-  flex: 1;
-}
-}
-`;
-
-const ProjectInfo = styled.div`
-/* border: 2px solid blue; */
-margin-left: 45px;
-max-width: 500px;
-color: white;
-@media (max-width : 768px) {
-  max-width: 100%;
-  margin-left: 15px;
-  margin-top: 100px;
-}
-
-h2 {
-  margin-top: 30px;
-  @media (max-width : 490px) {
-    margin-top: 90px;
-  }
-}
-
-h4 {
-  margin-top: 10px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  @media (max-width : 768px) {
-    padding: 2px;
-    margin: 10px 10px;
-  }
-}
-
-p {
-  text-align: justify;
-  @media (max-width : 768px) {
-    padding: 2px;
-    margin: 0 20px;
-  }
-}
-`;
-
-const Icons = styled.img`
-/* border: 2px solid pink; */
-height: 25px;
-margin: 5px;
-@media (max-width : 768px) {
-  height: 20px;
-}
-`;
